39fb8ddb3bb40d0275c5feea516fb81ab5a04dce
Idevaldo De Lira <ili@scireum.de>
2019-11-18 10:57:59 +0100

    Code Quality. Removing instance.conf from VCS

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-printf "
-http.port=\"80\"
+printf "http.port=80
 sirius.autoSetup=true
 http.sessionSecret=\"TEST\"
 client.server=\"localhost:80\"
 product.baseUrl=\"http://localhost:80\"
-redis.pools.system.host=\"$REDIS_HOST\"
-redis.pools.system.port=\"$REDIS_PORT\"
+redis.pools.system.host=\"%s\"
+redis.pools.system.port=%s
 elasticsearch {
-    hosts=\"$ES_HOST\"
-}" >instance.conf
+    hosts=\"%s\"
+}" "$REDIS_HOST" "$REDIS_PORT" "$ES_HOST" >instance.conf
+
 java -cp sirius-chat-sample-DEVELOPMENT-SNAPSHOT.jar:"release-dir/lib/*" sirius.kernel.Setup

bde60952d1648eb4399c09ba878e4ad03e2139c2
Idevaldo De Lira <ili@scireum.de>
2019-11-30 13:16:26 +0100

    Adds IntelliJ .idea project data

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
+#!/bin/sh
 printf "http.port=80
 sirius.autoSetup=true
-http.sessionSecret=\"TEST\"
+http.sessionSecret=\"S3cret\"
 client.server=\"localhost:80\"
 product.baseUrl=\"http://localhost:80\"
 redis.pools.system.host=\"%s\"
 redis.pools.system.port=%s
-elasticsearch {
-    hosts=\"%s\"
-}" "$REDIS_HOST" "$REDIS_PORT" "$ES_HOST" >instance.conf
+elasticsearch.hosts=\"%s\"
+" "$REDIS_HOST" "$REDIS_PORT" "$ES_HOST" >instance.conf
 
 java -cp sirius-chat-sample-DEVELOPMENT-SNAPSHOT.jar:"release-dir/lib/*" sirius.kernel.Setup
